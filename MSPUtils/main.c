1b13a0090e42c1088d233519e31832ffb3f4bf37
Mac Source Ports <macsourceports@gmail.com>
2022-07-01 17:36:57 -0500

    Added three basic methods

---
+++ MSPUtils/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  MSPUtils
+//
+//  Created by Tom Kidd on 6/29/22.
+//
+
+#include <stdio.h>
+#import "msputils.h"
+
+int main(int argc, const char * argv[]) {
+    printf("main start\n");
+    const char* appSupportDirectory = getAppSupportDirectory("dnf");
+    printf("appSupportDirectory: %s\n", appSupportDirectory);
+    
+    const char* bundlePath = getBundlePath();
+    printf("bundlePath: %s\n", bundlePath);
+    
+    displayMessageBox("Info", "This is an info message", 0);
+    displayMessageBox("ERROR", "This is an ERROR message", 1);
+    printf("main end\n");
+
+    return 0;
+}
---
+++ MSPUtils/msputils.h
@@ -0,0 +1,15 @@
+//
+//  msputils.h
+//  MSPUtils
+//
+//  Created by Tom Kidd on 6/29/22.
+//
+
+#ifndef msputils_h
+#define msputils_h
+
+const char* getAppSupportDirectory(const char* subdir);
+void displayMessageBox(const char* title, const char* message, int isError);
+const char* getBundlePath(void);
+
+#endif /* msputils_h */
